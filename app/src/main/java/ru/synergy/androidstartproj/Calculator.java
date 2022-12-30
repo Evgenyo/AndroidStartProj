5192678d178dd9e8e3647ae5b0f6d43ed706f06d
Evgenyo <oleme85@gmail.com>
2022-12-30 15:09:43 +0700

    Our first calculator done

--- app/src/main/java/ru/synergy/androidstartproj/Calculator.java
+++ app/src/main/java/ru/synergy/androidstartproj/Calculator.java
@@ -24,6 +24,7 @@ public class Calculator extends AppCompatActivity {
         calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                calcuclateAnswe();
 
             }
         });
