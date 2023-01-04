55ed3a9e2482637d1473b7e29c6be2c0210ac01e
Evgenyo <oleme85@gmail.com>
2023-01-04 21:48:22 +0700

    Our first calculator activity

--- app/src/main/java/ru/synergy/androidstartproj/Calculator.java
+++ app/src/main/java/ru/synergy/androidstartproj/Calculator.java
@@ -3,6 +3,7 @@ package ru.synergy.androidstartproj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,9 +15,15 @@ import org.w3c.dom.Text;
 
 public class Calculator extends AppCompatActivity {
 
+    private static final String LogcatTag = "CALCULATOR_ACTIVITY";
+    private static final String LifecycleTag = "LIFECYCLE";
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(LifecycleTag, "I'm on create, and i'm started");
         setContentView(R.layout.activity_calculator2);
 
         final Button calculate = (Button) findViewById(R.id.calc);
@@ -29,6 +36,37 @@ public class Calculator extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LifecycleTag,"I'm onPause, I'm started");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LifecycleTag, "I'm onResume, I'm started");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LifecycleTag, "I'm onStop, I'm started");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LifecycleTag, "I'm on Destroy, I'm started");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LifecycleTag, "I'm onStart, and I'm startred");
+    }
+
     private void calcuclateAnswe(){
             EditText numOne = (EditText) findViewById(R.id.editTextNumberDecimal);
             EditText numTwo = (EditText) findViewById(R.id.editTextNumberDecimal2);
