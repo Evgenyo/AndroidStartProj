3b3dcdc003afb469e0192363fef48fd8f7f5097b
Evgenyo <oleme85@gmail.com>
2023-01-09 21:55:22 +0700

    First intent to other activity

--- app/src/main/java/ru/synergy/androidstartproj/Calculator.java
+++ app/src/main/java/ru/synergy/androidstartproj/Calculator.java
@@ -2,6 +2,7 @@ package ru.synergy.androidstartproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
@@ -48,6 +49,8 @@ public class Calculator extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 calcuclateAnswe();
+                Intent i = new Intent(Calculator.this,MainActivity.class);
+                startActivity(i);
 
             }
         });
